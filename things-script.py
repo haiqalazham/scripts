3c2503aecd9c602012beb55fb49cd3b629b7dd21
Haiqal Azham <85307334+haiqalazham@users.noreply.github.com>
2026-03-14 13:28:25 +0800

    Add anytime and upcoming to things-script.py

--- things-script.py
+++ things-script.py
@@ -39,10 +39,48 @@ def show_last_week():
         print()
 
 
+def show_upcoming():
+    tasks = things.upcoming()
+
+    grouped = defaultdict(list)
+
+    for t in tasks:
+        if t.get("start_date"):
+            day = t["start_date"].split()[0]
+            grouped[day].append(t["title"])
+
+    print("Upcoming tasks:\n")
+    for day in sorted(grouped):
+        print(day)
+        for title in grouped[day]:
+            print(f"- {title}")
+        print()
+
+
+def show_anytime():
+    tasks = things.anytime()
+
+    grouped = defaultdict(list)
+
+    for t in tasks:
+        if t.get("area_title"):
+            area = t["area_title"]
+            grouped[area].append(t["title"])
+
+    print("Anytime tasks:\n")
+    for area in sorted(grouped):
+        print(area)
+        for title in grouped[area]:
+            print(f"- {title}")
+        print()
+
+
 def menu():
     print("Select an option:\n")
     print("1. Today's tasks")
-    print("2. Last week's completed tasks\n")
+    print("2. Upcoming tasks")
+    print("3. Anytime tasks")
+    print("4. Last week's completed tasks\n")
 
     choice = input("Enter choice: ").strip()
 
@@ -50,6 +88,12 @@ def menu():
         print()
         show_today()
     elif choice == "2":
+        print()
+        show_upcoming()
+    elif choice == "3":
+        print()
+        show_anytime()
+    elif choice == "4":
         print()
         show_last_week()
     else:
@@ -64,6 +108,10 @@ def main():
             show_today()
         elif arg in {"week", "w", "last-week"}:
             show_last_week()
+        elif arg in {"upcoming"}:
+            show_upcoming()
+        elif arg in {"anytime"}:
+            show_anytime()
         else:
             print("Unknown command")
     else:
